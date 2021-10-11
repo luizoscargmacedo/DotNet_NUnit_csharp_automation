296c1647fd2698cca3d2f7e1a1031991c765eb5d
Luiz Oscar Guabiraba de Macedo <luiz.gmacedo@gmail.com>
2021-10-11 12:32:45 -0300

    page objects included

---
+++ aut4CsharpNUnit/PageObject/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+
+namespace aut4CsharpNUnit.PageObject
+{
+    public class LoginPage
+    {
+
+        private RemoteWebDriver _driver;
+
+        public LoginPage(RemoteWebDriver driver) => _driver = driver;
+
+        IWebElement btnReturnLogin => _driver.FindElementById("returnLogin");
+        IWebElement txtEmailField => _driver.FindElementById("txtEmail");
+        IWebElement txtPasswordField => _driver.FindElementById("txtPassword");
+        IWebElement btnLogin => _driver.FindElementById("btnLogin");
+
+        public void ValidatingElementsOnReturnScreen()
+        {
+            Assert.IsTrue(btnReturnLogin.Enabled);
+        }
+
+        public void ClickOnLogin()
+        {
+            btnReturnLogin.Click();
+        }
+
+        public void ValidatingElementsOnLoginScreen()
+        {
+            Assert.IsTrue(btnLogin.Enabled);
+            Assert.IsTrue(txtEmailField.Enabled);
+            Assert.IsTrue(txtPasswordField.Enabled);
+        }
+
+        public void ExecuteLogin()
+        {
+            txtEmailField.SendKeys("luiz.test@test.com");
+            txtPasswordField.SendKeys("Spice#9976thor44");
+            btnLogin.Click();
+            Thread.Sleep(5000);
+        }
+
+
+    }
+}
---
+++ aut4CsharpNUnit/PageObject/MenuPage.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+
+namespace aut4CsharpNUnit.PageObject
+{
+    public class MenuPage
+    {
+
+        private RemoteWebDriver _driver;
+
+        public MenuPage(RemoteWebDriver driver) => _driver = driver;
+
+        IWebElement btnBuyNow => _driver.FindElementById("buy-button");
+        IWebElement txtEnterTheirName => _driver.FindElementById("user_name");
+
+        public void ValidatingElementsOnMenuScreen()
+        {
+            Assert.IsTrue(btnBuyNow.Enabled);
+        }
+
+        public void ValidateOptions()
+        {
+            
+            // JavaScript executor
+            BasicConfiguration.ExecuteJavaScript(_driver, "document.querySelector('body > app-root > app-start > div.secondary-container > div > div > div:nth-child(2) > div > div > p.secondary-card-title')");
+            Thread.Sleep(5000);
+            BasicConfiguration.ExecuteJavaScript(_driver, "document.querySelector('app-start > div.secondary-container > div > div > div:nth-child(3) > div > div > p.secondary-card-subtitle').click()");
+            Thread.Sleep(5000);
+        }
+
+        public void ValidateInviteYourColleaguesScreen()
+        {
+            Assert.IsTrue(txtEnterTheirName.Enabled);
+        }
+
+    }
+}
